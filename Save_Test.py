bf1eced2a8ef893df73b51a55ef7bd697c5455cf
shurfrousky <kriderbrand13@gmail.com>
2025-02-14 19:04:54 -0800

    added rend to roll_to_save funct.

--- Save_Test.py
+++ Save_Test.py
@@ -5,23 +5,33 @@ import AoS_Unit_Stats
 Test = Hit_Wound_Roll_Test
 UnitStats = AoS_Unit_Stats
 defending_unit_SAVE = UnitStats.Stormcast_Vindictors_Stats['SAVE']
+rend = UnitStats.Tzaangor_Enlightened_Disc['RND']
 
 #Rolls to save from incoming wounds
 def roll_to_save(save_amount, save_rating):
     import random
     failed_saves = []
     succesful_saves = []
+    save_const = save_rating
 
     for save in range(save_amount):
         d6 = random.randint(1, 6)
-    
+        save_rating = save_const
+
+        if rend >= 1:
+            save_rating += rend
+
         if d6 < save_rating:
             failed_saves.append(d6)
         elif d6 >= save_rating:
             succesful_saves.append(d6)
 
-    print(f"Rolling {save_amount} dice to save on a {defending_unit_SAVE}+...")
-    print(f"You rolled {succesful_saves}, {len(succesful_saves)} save! The rest, {failed_saves}, {len(failed_saves)} failed.")
+    if rend >= 1:
+        print(f"Rolling {save_amount} dice to save on a {defending_unit_SAVE + rend}+ (w/ rend)...")
+        print(f"You rolled {succesful_saves}, {len(succesful_saves)} save! The rest, {failed_saves}, {len(failed_saves)} failed.")
+    else:
+        print(f"Rolling {save_amount} dice to save on a {defending_unit_SAVE}+...")
+        print(f"You rolled {succesful_saves}, {len(succesful_saves)} save! The rest, {failed_saves}, {len(failed_saves)} failed.")
     return failed_saves
 
 # Calculates Damage
